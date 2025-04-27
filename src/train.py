5e94173b720f4c049563b8e3756c2080d9017f66
André Livingston Messina <bilaomon@gmail.com>
2025-04-27 19:28:52 -0300

    Adiciona loop de treino e validação, primeira versão da rede está pronta

--- src/train.py
+++ src/train.py
@@ -21,3 +21,47 @@ model = MLP(input_size, hidden_sizes, num_classes, dropout_rate=0.3)
 
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+num_epochs = 10
+
+for epoch in range(num_epochs):
+    model.train()
+    running_loss = 0.0
+
+    for inputs, labels in train_loader:
+        inputs = inputs.to(torch.float32)
+        labels = labels.long()
+
+        optimizer.zero_grad()             # Zera gradiente 
+        outputs = model(inputs)           # Forward
+        loss = criterion(outputs, labels) # Loss
+        loss.backward()                   # Backpropagation
+        optimizer.step()                  # Atualiza pesos
+
+        running_loss += loss.item()
+
+    avg_train_loss = running_loss / len(train_loader)
+
+    model.eval()
+    val_loss = 0.0
+    correct = 0
+    total = 0
+
+    with torch.no_grad():
+        for inputs, labels in val_loader:
+            inputs = inputs.to(torch.float32)
+            labels = labels.long()
+
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+
+            val_loss += loss.item()
+
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    avg_val_loss = val_loss / len(val_loader)
+    val_accuracy = 100 * correct / total
+
+    print(f"Epoch [{epoch+1} / {num_epochs}], Loss Treino: {avg_train_loss:.4f}, Loss Val: {avg_val_loss:.4f}, Acurácia Val: {val_accuracy:.2}%")
