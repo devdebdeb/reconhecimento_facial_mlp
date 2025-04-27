ab86a17d4924b70277509994597b72e0d5241133
André Livingston Messina <bilaomon@gmail.com>
2025-04-27 19:13:29 -0300

    Adiciona separação train/val, instancia o modelo e estabelece criterion e optimizer

--- src/train.py
+++ src/train.py
@@ -8,4 +8,16 @@ from model import MLP
 
 df = FaceDataset('../processed_dataset')
 train_size = int(0.85 * len(df))
+val_size = len(df) - train_size
 train_dataset, val_dataset = random_split(df, [train_size, val_size])
+
+train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
+val_loader = DataLoader(val_dataset, batch=32, shuffle=False)
+
+input_size = 2304
+hidden_sizes = [512, 128]
+num_classes = 4
+model = MLP(input_size, hidden_sizes, num_classes, dropout_rate=0.3)
+
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=0.001)
